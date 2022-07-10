0b06361671da4892407928793969b793f39c0bda
Ray <rei.piyo.55687@gmail.com>
2022-07-10 18:28:46 +0900

    背景リピート

--- Assets/Scripts/StageRepeat.cs
+++ Assets/Scripts/StageRepeat.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class StageRepeat : MonoBehaviour
 {
-    Vector3 _startPos;
-    float _repeatWidth;
+    [Tooltip("現在の座標")] Vector3 _currentPosition;
+    [Tooltip("どのくらい離れたらリピートするか")] float _repeatDistance;
+    [Tooltip("移動先の座標")] Vector3 _nextPosition;
+    [SerializeField, Header("プレイヤー")] GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        _startPos = transform.position;
-        _repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        _currentPosition = transform.position;
+        _repeatDistance = GetComponent<BoxCollider>().size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         // If background moves left by its repeat width, move it back to start position
-        if (transform.position.x < _startPos.x - _repeatWidth)
+        if (player.transform.position.x - _currentPosition.x > _repeatDistance)
         {
-            transform.position = _startPos;
+            _nextPosition = new Vector3(_currentPosition.x + 30f, _currentPosition.y, _currentPosition.z);
+            transform.position = _nextPosition;
+            _currentPosition = transform.position;
         }
     }
 
